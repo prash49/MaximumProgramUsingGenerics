0c95fcb58b9539e9aaf4e7578390ed7e8fd33966
Prashanth <imprash4u@gmail.com>
2021-09-07 20:31:55 +0530

    Added: Refactored program to get maximum of 3 values of any primitive dataType

--- src/main/java/com/bridgelabz/maximumusinggenerics/Maximum.java
+++ src/main/java/com/bridgelabz/maximumusinggenerics/Maximum.java
@@ -1,21 +1,39 @@
 package com.bridgelabz.maximumusinggenerics;
 
 /**
- * Hello world!*/
-public class Maximum {
-    public static String getMaximum(String str1, String str2, String str3) {
-        String max = str1;
-        if (str2.compareTo(str1) > 0 && str2.compareTo(str3) > 0) {
-            max = str2;
-        } else if (str3.compareTo(str1) > 0 && str3.compareTo(str2) > 0) {
-            max = str3;
+ * Hello world!
+ */
+public class Maximum<E extends Comparable> {
+    E value1, value2, value3;
+
+    public Maximum(E value1, E value2, E value3) {
+        this.value1 = value1;
+        this.value2 = value2;
+        this.value3 = value3;
+    }
+
+    public E getMaximum() {
+        E maximum = getMaximum(value1, value2, value3);
+        return maximum;
+    }
+
+    public static <E extends Comparable> E getMaximum(E value1, E value2, E value3) {
+        E max = value1;
+        if (value2.compareTo(value3) > 0 && value2.compareTo(value3) > 0) {
+            max = value2;
+        } else if (value3.compareTo(max) > 0) {
+            max = value3;
         }
+        toPrint(value1, value2, value3, max);
         return max;
+    }
+    private static  <E> void toPrint(E value1, E value2, E value3, E max){
+        System.out.println("Max of "+value1+" ,"+value2+" ,"+value3+" is : "+max);
     }
 
     public static void main(String[] args) {
-        System.out.println("Welcome to Maximum Program Using Generics");
-        String str1 = "Aa", str2 = "Bb", str3 = "Cc";
-        System.out.println(getMaximum(str1, str2, str3));
+        Maximum.getMaximum(12,13,15);
+        Maximum.getMaximum("String","string","STRING");
+        Maximum.getMaximum(12.2,14.5,00.2);
     }
 }
