10cbefe2b240f756d2ce59c12bacff41fa543fa5
Prashanth <imprash4u@gmail.com>
2021-09-07 15:57:33 +0530

    Added: Welcome Message and Project Structure

---
+++ src/main/java/com/bridgelabz/maximumusinggenerics/Maximum.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.maximumusinggenerics;
+
+/**
+ * Hello world!
+ *
+ */
+public class Maximum
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Maximum Program Using Generics" );
+    }
+}
---
+++ src/test/java/com/bridgelabz/maximumusinggenerics/MaximumTest.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.maximumusinggenerics;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class MaximumTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
