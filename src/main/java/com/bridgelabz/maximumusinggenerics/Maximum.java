a39a175ae129ab256610a6b79b2d8f19c320cf88
Prashanth <imprash4u@gmail.com>
2021-09-07 16:42:44 +0530

    Added:Program to Find Maximum of 3 Floats

--- src/main/java/com/bridgelabz/maximumusinggenerics/Maximum.java
+++ src/main/java/com/bridgelabz/maximumusinggenerics/Maximum.java
@@ -4,8 +4,8 @@ package com.bridgelabz.maximumusinggenerics;
  * Hello world!
  */
 public class Maximum {
-    public static Integer getMaximum(Integer num1, Integer num2, Integer num3) {
-        Integer max = num1;
+    public static Float getMaximum(Float num1, Float num2, Float num3) {
+        Float max = num1;
         if (num2 > num1 && num2 > num3) {
             max = num2;
         } else if (num3 > num1 && num3 > num2) {
@@ -16,7 +16,7 @@ public class Maximum {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Maximum Program Using Generics");
-        Integer num1 = 10, num2 = 15, num3 = 12;
-        System.out.println(getMaximum(10,12,15));
+        Float num1 = 10f, num2 = 15f, num3 = 12f;
+        System.out.println(getMaximum(num1, num2, num3));
     }
 }
--- src/test/java/com/bridgelabz/maximumusinggenerics/MaximumTest.java
+++ src/test/java/com/bridgelabz/maximumusinggenerics/MaximumTest.java
@@ -8,9 +8,13 @@ import org.junit.Test;
  * Unit test for simple App.
  */
 public class MaximumTest {
-    public void given3Values_WhenGetMaximum_ShouldReturnCorrectValue() {
-        int actualResult = Maximum.getMaximum(10, 15, 12);
-        Assert.assertEquals(15, actualResult);
+//    public void given3Values_WhenGetMaximum_ShouldReturnCorrectValue() {
+//        int actualResult = Maximum.getMaximum(10, 15, 12);
+//        Assert.assertEquals(15, actualResult);
+//    }
+    public void given3Values_WhenGetMaximum_ShouldReturnCorrectValue(){
+        float actualResult = Maximum.getMaximum(10f,15f,12f);
+                Assert.assertEquals(15, actualResult ,0);
     }
 
 }
